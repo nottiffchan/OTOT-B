70e92639056d71cf9a4ebcc505ee82ffabfab598
nottiffchan <nottiffchan@gmail.com>
2022-09-04 03:27:04 +0800

    Add cors to backend

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ let mongoose = require("mongoose");
 // Initialise the app
 let app = express();
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions)); // Use this after the variable declaration
+
 // Import routes
 let apiRoutes = require("./api-routes");
 // Configure bodyparser to handle post requests
