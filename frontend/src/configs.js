b333573ad2695126e57e063bec839e93ae73bd44
nottiffchan <nottiffchan@gmail.com>
2022-09-17 01:58:22 +0800

    Update endpoint

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -2,4 +2,4 @@ export const API_URL =
   // process.env.REACT_APP_ENVIRONMENT === "PROD"
   //   ? process.env.REACT_APP_PROD_BACKEND
   //   : process.env.REACT_APP_DEV_BACKEND;
-  "https://k37yrws3ig.execute-api.ap-southeast-1.amazonaws.com/dev/";
+  "https://9ri3uehn56.execute-api.ap-southeast-1.amazonaws.com/dev/";
